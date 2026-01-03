008c9103b471d89409e4fefbdc35d6cfa82442b3
NELSON BARRETO <nelsonbarreto@MacBook-Air-5.local>
2026-01-02 20:07:54 -0400

    Fix missing closing div tag in chat widget

--- src/components/AtencioChatWidget.jsx
+++ src/components/AtencioChatWidget.jsx
@@ -240,7 +240,8 @@ function AtencioChatWidget() {
                   }
                 )
               }}
-            />
+            ></div>
+          </div>
         ))}
         {loading && (
           <div
